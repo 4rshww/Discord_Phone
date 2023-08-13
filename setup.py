46371b84e807206a67aeaf070f61b437af841a6d
4rshww <102831165+4rshww@users.noreply.github.com>
2023-08-14 01:12:30 +0330

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="discord_ios",
+    version="1.1.1",
+    packages=find_packages(),
+    include_package_data=True,
+    license="MIT License",
+    description="get your discord.py bots appear as like its on a phone.",
+    keywords="discord.py, discord ios, discord ios status, discord bot mobile status, discord mobile bot status",
+    url="https://github.com/",
+    author="dropout1337",
+    author_email="ouglasord@gmail.com",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ]
+)
